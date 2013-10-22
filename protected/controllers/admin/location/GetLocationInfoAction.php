b206694735906a943f3839da48ac4d5624fabe12
Matthew Scaperoth <mscapero@gwu.edu>
2013-10-22 08:13:06 -0400

    fixed update location issue Signed-off-by:Matthew Scaperoth <mscapero@gwu.edu>

--- protected/controllers/admin/location/GetLocationInfoAction.php
+++ protected/controllers/admin/location/GetLocationInfoAction.php
@@ -5,7 +5,7 @@ class GetLocationinfoAction extends CAction {
     public function run() {
             $request = $_POST['UpdateLocation'];
             $loc_id = $request['locationnameupdate'];
-            
+            $return_data = 'Location not found';
             $db_rows = Ips::model()->findAll();
             foreach($db_rows as $row){
                 if(md5($row['ip_loc_id'])==$loc_id){
@@ -16,9 +16,10 @@ class GetLocationinfoAction extends CAction {
                     echo json_encode($return_data);
                     break;
                 }
+                else echo json_encode($return_data);
             }
             
-        
+            
     }
 
 }
